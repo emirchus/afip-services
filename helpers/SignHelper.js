9f8a7d2d2ca40d90cfdaa719712638af53c1b5b2
Emir Ali <emirhassan.ali02@gmail.com>
2024-07-15 15:43:06 -0300

    feat: arreglo algo jeje

--- helpers/SignHelper.js
+++ helpers/SignHelper.js
@@ -1,5 +1,8 @@
-var util = require('util'),
-	spawn = require('child_process').spawn;
+var util = require("util"),
+	spawn = require("child_process").spawn;
+const path = require("path");
+
+const fs = require("fs");
 
 // Expose methods.
 exports.sign = sign;
@@ -17,39 +20,51 @@ exports.sign = sign;
 function sign(options) {
 	return new Promise(function (resolve, reject) {
 		options = options || {};
+		if (!options.content) reject("Invalid content.");
 
-		if (!options.content)
-			reject('Invalid content.');
+		if (!options.key) reject("Invalid key.");
 
-		if (!options.key)
-			reject('Invalid key.');
+		if (!options.cert) reject("Invalid certificate.");
 
-		if (!options.cert)
-			reject('Invalid certificate.');
+		const date = new Date().getTime();
+
+		const tempPemPath = path.join(__dirname, `temp.pem`);
+		const tempKeyPath = path.join(__dirname, `temp.key`);
+
+		fs.writeFileSync(tempPemPath, options.cert);
+		fs.writeFileSync(tempKeyPath, options.key);
 
 		var command = util.format(
-			'openssl smime -sign -signer %s -inkey %s -outform DER -nodetach',
-			options.cert,
-			options.key
+			"openssl smime -sign -signer %s -inkey %s -outform DER -nodetach",
+			tempPemPath,
+			tempKeyPath
 		);
 
-		if (options.password)
-			command += util.format(' -passin pass:%s', options.password);
 
-		var args = command.split(' ');
-		var child = spawn(args[0], args.splice(1), { encoding: 'base64' });
+		if (options.password)
+			command += util.format(" -passin pass:%s", options.password);
+
+		var args = command.split(" ");
+
+		console.log(args[0], args.splice(1));
+		var child = spawn(args[0], args.splice(1), { encoding: "base64" });
 
 		var der = [];
 
-		child.stdout.on('data', function (chunk) {
+		child.stdout.on("data", function (chunk) {
 			der.push(chunk);
 		});
 
-		child.on('close', function (code) {
+		child.on("close", function (code) {
+			console.log(code);
+
+			// fs.unlinkSync(tempPemPath);
+			// fs.unlinkSync(tempKeyPath);
+
 			if (code !== 0) {
-				reject(new Error('Process failed.'));
+				reject(new Error("Process failed."));
 			} else {
-				resolve(Buffer.concat(der).toString('base64'));
+				resolve(Buffer.concat(der).toString("base64"));
 			}
 		});
 
